c3c72c3fbccf8258a27fae07541e6e093b1925a5
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-10-29 16:02:25 +0000

    Updating to latest FlutterFlow output.

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -74,7 +74,7 @@ class FFAppState extends ChangeNotifier {
 
   late SharedPreferences prefs;
 
-  String _varVersao = '1.0.9';
+  String _varVersao = '1.0.10';
   String get varVersao => _varVersao;
   set varVersao(String value) {
     _varVersao = value;
